27aaf9d7ba11e487fdb71d45ca5fe346ff38d415
Jerry <oc3ansavenue@gmail.com>
2018-08-28 00:24:06 -0700

    Default map, added component
    - Fixed project settings Default Map
    - Added GetObjectName component (possibly redundant)
    - Added log message to PositionReporter

--- Source/EscapeGame1/GetObjectName.cpp
+++ Source/EscapeGame1/GetObjectName.cpp
@@ -1,10 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Jerry Lau 2018
 
-#include "PositionReporter.h"
+#include "GetObjectName.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UGetObjectName::UGetObjectName()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,17 +15,20 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UGetObjectName::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position reporter reporting for duty."));
+	FString ObjectName;
+	ObjectName = this->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("ObjectName"));
 	
 }
 
- 
+
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGetObjectName::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
